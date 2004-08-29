4a705d1393d140cce0755079650f7d2b4c1eea8d
slamb <slamb@slamb.org>
2004-08-29 04:59:53 +0000

    Show Ethernet media type.

--- NetGrowlerController.m
+++ NetGrowlerController.m
@@ -6,6 +6,15 @@
 //  Copyright (c) 2004 __MyCompanyName__. All rights reserved.
 //
 
+// Media stuff
+#import <sys/socket.h>
+#import <sys/sockio.h>
+#import <sys/ioctl.h>
+#import <net/if.h>
+#import <net/if_media.h>
+#import <unistd.h>
+
+// Rest of the includes
 #import "NetGrowlerController.h"
 #import <GrowlAppBridge/GrowlApplicationBridge.h>
 #import <GrowlAppBridge/GrowlDefines.h>
@@ -24,12 +33,16 @@ static NSString *IP_APP_NAME				= @"Internet Connect.app";
 
 static NSString *APP_NAME					= @"NetGrowler.app";
 
+static struct ifmedia_description ifm_subtype_ethernet_descriptions[] = IFM_SUBTYPE_ETHERNET_DESCRIPTIONS;
+static struct ifmedia_description ifm_shared_option_descriptions[] = IFM_SHARED_OPTION_DESCRIPTIONS;
+
 @interface NetGrowlerController (PRIVATE)
 - (void)registerGrowl:(void*)context;
 - (void)applicationDidFinishLaunching:(NSNotification*)notification;
 - (void)linkStatusChange:(NSDictionary*)newValue;
 - (void)ipAddressChange:(NSDictionary*)newValue;
 - (void)airportStatusChange:(NSDictionary*)newValue;
+- (NSString*)getMediaForInterface:(NSString*)anInterface;
 @end
 
 @implementation NetGrowlerController
@@ -127,20 +140,22 @@ static NSString *APP_NAME					= @"NetGrowler.app";
 	NSDictionary *noteDict = nil;
 
 	if (active) {
+		NSString *media = [self getMediaForInterface:@"en0"];
+		NSString *desc = [NSString stringWithFormat:@"Interface:\ten0\nMedia:\t%@", media];
 		NSLog(@"Ethernet cable plugged");
 		noteDict = [NSDictionary dictionaryWithObjectsAndKeys:
 			NOTE_LINK_UP, GROWL_NOTIFICATION_NAME,
 			APP_NAME, GROWL_APP_NAME,
-			@"Ethernet active", GROWL_NOTIFICATION_TITLE,
-			[NSString stringWithFormat:@"Gained wired connection."], GROWL_NOTIFICATION_DESCRIPTION,
+			@"Ethernet activated", GROWL_NOTIFICATION_TITLE,
+			desc, GROWL_NOTIFICATION_DESCRIPTION,
 			[ipIcon TIFFRepresentation], GROWL_NOTIFICATION_ICON,
 			nil];
 	} else {
 		noteDict = [NSDictionary dictionaryWithObjectsAndKeys:
 			NOTE_LINK_DOWN, GROWL_NOTIFICATION_NAME,
 			APP_NAME, GROWL_APP_NAME,
-			@"Ethernet inactive", GROWL_NOTIFICATION_TITLE,
-			[NSString stringWithFormat:@"Lost wired connection."], GROWL_NOTIFICATION_DESCRIPTION,
+			@"Ethernet deactivated", GROWL_NOTIFICATION_TITLE,
+			[NSString stringWithFormat:@"Interface:\ten0"], GROWL_NOTIFICATION_DESCRIPTION,
 			[ipIcon TIFFRepresentation], GROWL_NOTIFICATION_ICON,
 			nil];
 	}		
@@ -217,6 +232,76 @@ static NSString *APP_NAME					= @"NetGrowler.app";
 																   object:nil
 																 userInfo:noteDict];
 	airportStatus = [newValue retain];
+}
+
+- (NSString*) getMediaForInterface:(NSString*)anInterface {
+	// This is all made by looking through Darwin's src/network_cmds/ifconfig.tproj.
+	// There's no pretty way to get media stuff; I've stripped it down to the essentials
+	// for what I'm doing.
+
+	NSAssert([anInterface cStringLength] < IFNAMSIZ, @"Interface name too long");
+
+	int s = socket(AF_INET, SOCK_DGRAM, 0);
+	NSAssert(s >= 0, @"Can't open datagram socket");
+	struct ifmediareq ifmr;
+	memset(&ifmr, 0, sizeof(ifmr));
+	strncpy(ifmr.ifm_name, [anInterface cString], [anInterface cStringLength]);
+
+	/*
+	 * First round.
+	 * Make the request, see if it passes. (Some interfaces have no media information.)
+	 * Find out how much storage space we need.
+	 */
+	if (ioctl(s, SIOCGIFMEDIA, (caddr_t)&ifmr) < 0) {
+		// Media not supported.
+		close(s);
+		return nil;
+	}
+	
+#if 0
+	int *media_list = (int*) malloc(ifmr.ifm_count * sizeof(int));
+	NSAssert(@"Memory allocation failure", media_list != NULL);
+	ifmr.ifm_ulist = media_list;
+			
+	/*
+	 * Second round.
+	 * Now it should actually fill in the information.
+	 */
+	int retval = ioctl(s, SIOCGIFMEDIA, (caddr_t)&ifmr);
+	NSAssert(@"Unexpected SIOCGIFMEDIA failure", retval >= 0);
+#endif
+	close(s);
+
+	// Now ifmr.ifm_current holds the selected type (probably auto-select)
+	// ifmr.ifm_active holds details (100baseT <full-duplex> or similar)
+	// We only want the ifm_active bit.
+
+	const char *type = "Unknown";
+
+	// We'll only look in the Ethernet list. I don't care about anything else.
+	struct ifmedia_description *desc;
+	for (desc = ifm_subtype_ethernet_descriptions; desc->ifmt_string != NULL; desc++) {
+		if (IFM_SUBTYPE(ifmr.ifm_active) == desc->ifmt_word) {
+			type = desc->ifmt_string;
+			break;
+		}
+	}
+
+	NSString *options = nil;
+	
+	// And fill in the duplex settings.
+	for (desc = ifm_shared_option_descriptions; desc->ifmt_string != NULL; desc++) {
+		if (ifmr.ifm_active & desc->ifmt_word) {
+			if (options == nil) {
+				options = [NSString stringWithCString:desc->ifmt_string];
+			} else {
+				options = [NSString stringWithFormat:@"%@,%s", options, desc->ifmt_string];
+			}
+		}
+	}
+	
+	return (options == nil) ? [NSString stringWithCString:type]
+							: [NSString stringWithFormat:@"%s <%@>", type, options];
 }
 
 @end
